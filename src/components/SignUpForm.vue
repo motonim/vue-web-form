35877f564b5501cf0040e6f7ef7a2581c61e002c
Jaeri Park <jaeri.park@umontreal.ca>
2024-01-15 16:18:19 -0500

    add skills and show the array

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -13,12 +13,18 @@
         <option value="designer">Web Designer</option>
     </select>
 
+    <label for="">Skills</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill">
+        {{  skill }}
+    </div>
+
     <div class="terms">
         <input type="checkbox" v-model="terms" required>
         <label for="">Accept termps and conditions</label>
     </div>
 
-    <div class="">
+    <!-- <div class="">
         <input type="checkbox" value="shaun" v-model="names" >
         <label for="">Shaun</label>
     </div>
@@ -29,14 +35,14 @@
     <div class="">
         <input type="checkbox" value="mario" v-model="names" >
         <label for="">Mario</label>
-    </div>
+    </div> -->
   </form>
   
     <p>Email: {{  email  }}</p>
     <p>Password: {{  password  }}</p>
     <p>Role: {{  role  }}</p>
     <p>Terms accepted: {{  terms  }}</p>
-    <p>Names: {{  names  }}</p>
+    <p>Skills: {{  skills  }}</p>
 </template>
 
 <script>
@@ -47,9 +53,22 @@ export default {
             password: '',
             role: '',
             terms: false,
-            names: []
+            // names: [],
+            tempSkill: '',
+            skills: []
         }
     },
+    methods: {
+        addSkill(e) {
+            if(e.key === ',' && this.tempSkill) {
+                this.tempSkill = this.tempSkill.slice(0, -1);
+                if (!this.skills.includes(this.tempSkill)) {
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ''
+            }
+        }
+    }
 }
 </script>
 

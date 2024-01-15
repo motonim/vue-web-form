f0229b7d1d699059493acc3ed4f3f925527eb7c5
Jaeri Park <jaeri.park@umontreal.ca>
2024-01-15 16:09:49 -0500

    example of an array

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -3,12 +3,40 @@
     <label for="">Email:</label>
     <input type="email" required v-model="email">
     <!-- v-model binds data in two ways, meaning in this case, input and data are connected so if you changed values inside data, it will be affected to input and vice versa. -->
+    
     <label for="">Password:</label>
     <input type="password" required v-model="password">
 
+    <label for="">Role:</label>
+    <select name="" id="" v-model="role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+    </select>
+
+    <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+        <label for="">Accept termps and conditions</label>
+    </div>
+
+    <div class="">
+        <input type="checkbox" value="shaun" v-model="names" >
+        <label for="">Shaun</label>
+    </div>
+    <div class="">
+        <input type="checkbox" value="yoshi" v-model="names" >
+        <label for="">Yoshi</label>
+    </div>
+    <div class="">
+        <input type="checkbox" value="mario" v-model="names" >
+        <label for="">Mario</label>
+    </div>
+  </form>
+  
     <p>Email: {{  email  }}</p>
     <p>Password: {{  password  }}</p>
-  </form>
+    <p>Role: {{  role  }}</p>
+    <p>Terms accepted: {{  terms  }}</p>
+    <p>Names: {{  names  }}</p>
 </template>
 
 <script>
@@ -16,7 +44,10 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            role: '',
+            terms: false,
+            names: []
         }
     },
 }
@@ -40,7 +71,7 @@ export default {
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input {
+  input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -48,5 +79,12 @@ export default {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
   }
 </style>
